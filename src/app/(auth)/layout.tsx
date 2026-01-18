5d2b19c05fc9496e7de441edf0914c384bbc4e16
Fikir Ayele <fikirayele332@gmail.com>
2026-01-18 16:45:56 +0000

    i inserted co.webp pic ture in public folder so use it for signup and lo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,11 +6,17 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-primary/5 p-4">
-        <div className="absolute top-8 left-8">
-            <Logo />
-        </div>
-      {children}
+    <div 
+      className="relative flex min-h-screen flex-col items-center justify-center bg-cover bg-center p-4"
+      style={{ backgroundImage: "url('/co.webp')" }}
+    >
+      <div className="absolute inset-0 bg-black/60 z-0" />
+      <div className="absolute top-8 left-8 z-10 [&_span]:text-white">
+          <Logo />
+      </div>
+      <div className="relative z-10">
+        {children}
+      </div>
     </div>
   );
 }

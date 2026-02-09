55018cfda130d2891ab6ce1f66d7f2e636c9aede
Fikir Ayele <fikirayele332@gmail.com>
2026-02-09 13:44:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8924998909-63eea",
+  "appId": "1:152545224061:web:723432d198f7703aab9814",
+  "apiKey": "AIzaSyDBHErYzhscnwGWf9EeioJiuoUT9NomD5o",
+  "authDomain": "studio-8924998909-63eea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "152545224061"
+};
